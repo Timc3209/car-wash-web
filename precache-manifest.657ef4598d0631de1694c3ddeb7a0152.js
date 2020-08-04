cbed5f37c1d2403b742f4637205a6d1daa102118
Timc3209 <timc3209@gmail.com>
2020-08-04 11:27:54 -0500

    Updates

--- precache-manifest.657ef4598d0631de1694c3ddeb7a0152.js
+++ precache-manifest.657ef4598d0631de1694c3ddeb7a0152.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "afb3546304ae4643123a2d7218e2ab76",
+    "revision": "ee6ca420fd38806a4ef163679bec9666",
     "url": "/car-wash-web/index.html"
   },
   {
-    "revision": "c859e9a3aa648ded8a3c",
+    "revision": "9515d4e957c12f8a026d",
     "url": "/car-wash-web/static/css/2.09d1976d.chunk.css"
   },
   {
-    "revision": "6914a7ea2761d7fdab03",
-    "url": "/car-wash-web/static/css/main.b65fdda8.chunk.css"
+    "revision": "9e782ac23cee926670c9",
+    "url": "/car-wash-web/static/css/main.1761104a.chunk.css"
   },
   {
-    "revision": "c859e9a3aa648ded8a3c",
-    "url": "/car-wash-web/static/js/2.a5fe96b7.chunk.js"
+    "revision": "9515d4e957c12f8a026d",
+    "url": "/car-wash-web/static/js/2.8cf43494.chunk.js"
   },
   {
     "revision": "99afe6ae89d1339713d7590d822fd3a8",
-    "url": "/car-wash-web/static/js/2.a5fe96b7.chunk.js.LICENSE.txt"
+    "url": "/car-wash-web/static/js/2.8cf43494.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6914a7ea2761d7fdab03",
-    "url": "/car-wash-web/static/js/main.9e907dfa.chunk.js"
+    "revision": "9e782ac23cee926670c9",
+    "url": "/car-wash-web/static/js/main.793a2b1e.chunk.js"
   },
   {
     "revision": "38bde0ca7b64425d2772",
